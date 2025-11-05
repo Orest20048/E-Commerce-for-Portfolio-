1f2af614202715c7e6af85fe608f6ce00ea87ab9
Ostap200488 <135761043+Ostap200488@users.noreply.github.com>
2025-11-05 19:34:54 -0330

    Update page.jsx

--- app/(public)/create-store/page.jsx
+++ app/(public)/create-store/page.jsx
@@ -34,8 +34,32 @@ export default function CreateStore() {
     }
 
     const fetchSellerStatus = async () => {
-        // Logic to check if the store is already submitted
-
+        const token = await getToken()
+        try {
+            const {data} =await axios.get('/api/store/create', {headers:{Authorization: `Bearer ${token}` }})
+            if(['approved' , 'rejected','pending'].includes(data.status)){
+                setStatus(data.status)
+                setAlreadySubmitted(true)
+                switch (data.status) {
+                    case "approved":
+                        setMessage("Your Store has been approved, you can new add products to your store from dashboard")
+                        setTimeout(()=>router.push("/store"), 5000)
+                        break;
+                    case "rejected":
+                        setMessage("Your store request has been rejected , contact the admin for more details ")
+                        break;
+                    case "pending":
+                        setMessage("Your store request is pending , please wait for admin to approve your store  ")
+                        break;
+                    default:
+                        break;
+                }
+            }else{
+                setAlreadySubmitted(false)
+            }
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
 
         setLoading(false)
     }
@@ -58,15 +82,18 @@ export default function CreateStore() {
 
 
             const { data } = await axios.post('/api/store/create', formData, {headers: { Authorization: `Bearer ${token}` }})
-        toast.success(data.message);
+        toast.success(data.message)
+        await fetchSellerStatus()
         } catch (error) {
         toast.error(error?.response?.data?.error || error.message);
     }
 }
 
     useEffect(() => {
-        fetchSellerStatus()
-    }, [])
+        if(user){
+           fetchSellerStatus() 
+        }
+    }, [user])
 
     if(!user){
         return(
